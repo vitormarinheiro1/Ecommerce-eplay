d058b2bd4b2a5fbb38803dc6718e3dd0b88cdf9a
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-07-05 13:03:28 -0300

    :sparkles: feat: Product e ProductList

---
+++ src/components/Product/styles.ts
@@ -0,0 +1,47 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { colors } from "../../styles";
+import { TagContainer } from "../Tag/styles";
+
+export const Card = styled(Link)`
+    background-color: ${colors.gray};
+    border-radius: 8px;
+    padding: 8px;
+    position: relative;
+    text-decoration: none;
+    color: ${colors.white};
+    display: block;
+    height: 100%;
+
+    img {
+        display: block;
+        width: 100%;
+        height: 250px;
+        object-fit: cover;
+    }
+
+    ${TagContainer} {
+        margin-right: 8px;
+    }
+`
+
+export const Title = styled.h3`
+    font-weight: bold;
+    font-size: 16x;
+    display: block;
+    margin-top: 16px;
+    margin-bottom: 8px;
+`
+
+export const Description = styled.p`
+    font-size: 14px;
+    line-height: 22px;
+    display: block;
+    margin-top: 16px;
+`
+
+export const Infos = styled.div`
+    position: absolute;
+    top: 16px;
+    right: 16px;
+`
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,28 @@
 import Banner from "../../components/Banner";
+import ProductsList from "../../components/ProductsList";
+import { useGetOnSaleQuery, useGetSoonQuery } from "../../services/api";
 
 const Home = () => {
+    const { data: onSaleGames, isLoading: isLoadingSale } = useGetOnSaleQuery()
+    const { data: soonGames, isLoading: isLoadingSoon } = useGetSoonQuery()
+
     return (
         <>
             <Banner />
+            <ProductsList
+                games={onSaleGames}
+                title="Promoções"
+                background="gray"
+                id="on-sale"
+                isLoading={isLoadingSale}
+            />
+            <ProductsList
+                games={soonGames}
+                title="Promoções"
+                background="gray"
+                id="on-sale"
+                isLoading={isLoadingSoon}
+            />
         </>
     )
 }
